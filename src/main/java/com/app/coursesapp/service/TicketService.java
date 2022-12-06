62494bfb721d06b187e2f894ddaa6e4bbf736b3a
Evgeny135 <75969458+Evgeny135@users.noreply.github.com>
2022-12-06 19:44:45 +0300

    WebApplication

---
+++ src/main/java/com/app/coursesapp/service/TicketService.java
@@ -0,0 +1,33 @@
+package com.app.coursesapp.service;
+
+import com.app.coursesapp.dao.TicketDao;
+import com.app.coursesapp.dto.TicketDto;
+
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class TicketService {
+
+    private static final TicketService INSTANCE = new TicketService();
+
+    private final TicketDao ticketDao = TicketDao.getInstance();
+
+    private TicketService() {
+    }
+
+    public List<TicketDto> findAllByFlightId(Long flightId) {
+        return ticketDao.findAllByFlightId(flightId).stream()
+                .map(ticket -> new TicketDto(
+                        ticket.getId(),
+                        ticket.getFlightId(),
+                        ticket.getSeatNo()
+                ))
+                .collect(toList());
+    }
+
+    public static TicketService getInstance() {
+        return INSTANCE;
+    }
+}
